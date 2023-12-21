1a5d83975d89de94682d83f0466251ed3d5f37b6
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2023-12-21 23:17:29 +0800

    get_absolut_json_path_v1

--- custom/extract_coco.py
+++ custom/extract_coco.py
@@ -1,26 +1,66 @@
 import os
 import sys
 from pathlib import Path
-
 from pycocotools.coco import COCO
 import requests
 
 
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]  # YOLOv5 root directory
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+def find_project_root(current_dir=None, marker_file=".gitignore"):
+    """
+    Find the project root directory based on the presence of a marker file.
+
+    :param current_dir: The starting directory for the search. If not provided, the current working directory is used.
+    :param marker_file: The name of the marker file that indicates the project root.
+    :return: The absolute path to the project root directory, or None if not found.
+    """
+    if current_dir is None:
+        current_dir = os.getcwd()
+
+    while True:
+        # Check if the marker file exists in the current directory
+        marker_path = os.path.join(current_dir, marker_file)
+        if os.path.isfile(marker_path):
+            return current_dir
+
+        # Move up one level in the directory hierarchy
+        parent_dir = os.path.dirname(current_dir)
+
+        # Break the loop if we have reached the root directory
+        if parent_dir == current_dir:
+            break
+
+        current_dir = parent_dir
+
+    return None
+
+
+def set_relative_path(relative_path):
+    """
+    Set the relative path based on the project root directory.
+
+    :param relative_path: The relative path to set.
+    """
+    # Find the project root
+    project_root = find_project_root()
+
+    if project_root is not None:
+        # Combine the project root and the relative path
+        absolute_path = os.path.join(project_root, relative_path)
+        return absolute_path
+    else:
+        raise RuntimeError("Project root not found.")
 
 
 def get_coco_subset(
-        coco_json_dir: Path,
+        coco_json_dir: str,
+        coco_json_name: str,
         classes: list,
         # output_dir: str
 ):
     # instantiate COCO specifying the annotations json path
-    path = Path(coco_json_dir)
-    coco = COCO(path)
+    json_relative_path = coco_json_dir + '/' + coco_json_name
+    json_path = set_relative_path(json_relative_path)
+    coco = COCO(json_path)
     # Specify a list of category names of interest
     catIds = coco.getCatIds(catNms=classes)
     # Get the corresponding image ids and images using loadImgs
@@ -38,7 +78,8 @@ def get_coco_subset(
 
 if __name__ == "__main__":
     get_coco_subset(
-        coco_json_dir=ROOT / 'data/json/instances_train2017.json',
+        coco_json_dir='data/json',
+        coco_json_name='instances_train2017.json',
         classes=['person']
         # output_dir='dataset/coco/image/train'
     )
