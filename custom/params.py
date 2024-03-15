177cf6b2972e9490af97a9c1a00503e190549a91
opdog <147395352+opdog@users.noreply.github.com>
2024-03-16 00:58:58 +0800

    训练和绘图封装，已经定义全局的路径

---
+++ custom/params.py
@@ -0,0 +1,25 @@
+"""
+参数模块
+  本模块用于指定训练和绘图的参数
+"""
+
+
+from settings import *
+
+
+train_params = {
+    "train_script": str(PROJECT_ROOT / "train.py"),
+    "weights": str(WEIGHTS / "weights"),
+    "cfg": str(MODELS / "yolov5s.yaml"),
+    "data": str(DATA / "test.yaml"),
+    "epochs": 5,
+    "batch_size": 16,
+}
+
+print_params = {
+    "path": str(TRAIN_RESULT / "exp" / "results.csv"),
+    "y_column": "     metrics/mAP_0.5",
+    "title": "Training Performance",
+    "xlabel": "epoch",
+    "ylabel": "mAP_0.5",
+}
